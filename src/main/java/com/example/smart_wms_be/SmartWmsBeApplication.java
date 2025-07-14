31bea64dba0710e11393f3e6ec81c06a3d08e838
Jinacker <rlawls1448@naver.com>
2025-07-14 13:38:34 +0900

    [FEAT] Spring Boot 프로젝트 생성 및 종속성 셋팅

---
+++ src/main/java/com/example/smart_wms_be/SmartWmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_wms_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWmsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartWmsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smart_wms_be/SmartWmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_wms_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWmsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
