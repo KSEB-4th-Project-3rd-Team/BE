546e4ffb7f1bd8065eff9931746819b83bb6278a
Jinacker <rlawls1448@naver.com>
2025-07-18 22:50:11 +0900

    [CHORE] DB_URL => AWS RDS 엔드포인트로 수정

--- src/main/java/com/example/smart_wms_be/SmartWmsBeApplication.java
+++ src/main/java/com/example/smart_wms_be/SmartWmsBeApplication.java
@@ -1,5 +1,6 @@
 package com.example.smart_wms_be;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartWmsBeApplication {
 
     public static void main(String[] args) {
+        // .env 파일 로드
+        Dotenv dotenv = Dotenv.load();
+        // 시스템 속성으로 설정
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        
         SpringApplication.run(SmartWmsBeApplication.class, args);
     }
 
