ecf31557e58d4684a404c433322fb6247aab3f00
Jinacker <rlawls1448@naver.com>
2025-07-18 17:44:34 +0900

    [CHORE] env 파일 제거

--- src/main/java/com/example/smart_wms_be/SmartWmsBeApplication.java
+++ src/main/java/com/example/smart_wms_be/SmartWmsBeApplication.java
@@ -2,16 +2,11 @@ package com.example.smart_wms_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv; // env 파일 로드용
 
 @SpringBootApplication
 public class SmartWmsBeApplication {
 
     public static void main(String[] args) {
-
-        Dotenv dotenv = Dotenv.load(); // env 파일 로드.
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
         SpringApplication.run(SmartWmsBeApplication.class, args);
     }
 
